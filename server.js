1d46dfca6468385a33145265cf5f3092a62573fa
Eric David <ego229@mail.com>
2020-02-15 13:18:59 -0500

    broken alerts for heroku

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require('express');
-
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+// Connect Database
 connectDB();
-
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/posts', require('./routes/api/posts'))
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-const port = process.env.PORT || 5000;
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-app.listen(port);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
